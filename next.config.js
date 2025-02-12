efb1ab7fb5e90cf31819203e9212beb621f62c2f
Amaninder Singh <amandeolofficial@gmail.com>
2025-02-12 11:35:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,33 @@
-// next.config.js
+const isLocal = process.env.NODE_ENV === 'development'; // Check if running locally
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/image-to-pdf',
+  assetPrefix: '/imagetopdf/', // Add this line
+  basePath: isLocal ? '' : '/imagetopdf', // Set basePath only for local development
+  trailingSlash: true, // Add this line
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/image-to-pdf/',
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
 
-module.exports = nextConfig
+    // Needed for pdf-lib WASM support
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
